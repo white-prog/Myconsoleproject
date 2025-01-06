2e189d22d35fbb0ec6cbeecb68803a9dfe382def
white-prog <sabinhameed55@gmail.com>
2025-01-07 00:18:51 +0530

    base .Net files

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, i just started to write in C#");
+Console.WriteLine("The current time is " + DateTime.Now);
+
+Console.Write("Please enter your name: ");
+string name = Console.ReadLine();
+Console.WriteLine("Hello, " + name);
